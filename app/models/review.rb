6df13b99aa811189f549a256d7c65d791590e59f
Arnd-Kisho Langenberg <aklangenberg@gmail.com>
2022-05-14 13:08:49 +0200

    relationship in models

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :booking
+  belongs_to :user
+  belongs_to :item, through: :booking
+
+  # validates :content, presence: true
+  # validates :rating, inclusion: { in: 0..5 }, numericality: { only_integer: true }
 end
