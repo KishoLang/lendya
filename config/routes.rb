1208f8007efb669f85521ce74922c6388afeb988
Arnd-Kisho Langenberg <aklangenberg@gmail.com>
2022-05-14 13:19:53 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :items do
+    resources :bookings, only: [:index, :show, :new, :create, :delete] do
+      resources :messages, only: [:index, :show, :new, :create]
+      resources :reviews, only: [:index, :show, :new, :create, :delete]
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
