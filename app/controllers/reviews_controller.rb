f1ea86971f2911297276826b33a1738dec28c4b6
Janek Iversen <janekiversen@gmail.com>
2022-05-28 16:43:54 +0200

    index finished, created review controller, and added index, new and create.Also created view review with simple form

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,25 @@
+class ReviewsController < ApplicationController
+
+  def index
+    @reviews = Review.all
+  end
+  def new
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @booking = Booking.find(params[:booking_id])
+    @review.booking = @booking
+    @review.save
+    redirect_to booking_path(@booking)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
+
+end
