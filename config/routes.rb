0326d4961c4a0980751ada66297a65236e8cf491
Janek Iversen <janekiversen@gmail.com>
2022-06-04 11:49:23 +0200

    fiexd

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :items do
-    resources :bookings, only: [:index, :new, :create ] do
-      resources :messages, only: [:index, :new, :create]
-      resources :reviews, only: [:index, :new, :create]
-    end
+    resources :bookings, only: [:index, :new, :create ]
   end
-
+    resources :bookings, only: [:show, :destroy ] do
+      resources :messages, only: [:index, :new, :create]
+      resources :reviews, only: [ :index, :new, :create]
+    end
   get "dashboard", to: "dashboard#show"
-  resources :bookings, only: [:show, :destroy]
   resources :messages, only: [:show]
   resources :reviews, only: [:show, :delete]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
