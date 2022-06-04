b8560de06117275975f9f7bff285ada842029b2c
Janek Iversen <janekiversen@gmail.com>
2022-06-04 15:50:38 +0200

    all to do with reviews and stars

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,7 +13,7 @@ class ReviewsController < ApplicationController
     @booking = Booking.find(params[:booking_id])
     @review = Review.new(review_params)
     @review.booking = @booking
-    if @review.save!
+    if @review.save
       redirect_to booking_path(@booking)
     else
       render "bookings/show"
